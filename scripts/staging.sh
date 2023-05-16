e39035e5d69d803388e8fba914bbd3e4221636a4
Andrei Kurilov <andrei.kurilov@lekane.com>
2023-05-16 09:30:06 +0300

    init

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -5,5 +5,5 @@ export ORG=awakari
 export COMPONENT=core-tests
 export SLUG=${REGISTRY}/${ORG}/${COMPONENT}
 export VERSION=latest
-docker tag awakari/api "${SLUG}":"${VERSION}"
+docker tag awakari/core-tests "${SLUG}":"${VERSION}"
 docker push "${SLUG}":"${VERSION}"
