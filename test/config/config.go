10ee7123713c061161c54a3e5ce180555427fb1c
Andrei Kurilov <andrei.kurilov@lekane.com>
2023-06-07 21:51:55 +0300

    save

--- test/config/config.go
+++ test/config/config.go
@@ -4,7 +4,7 @@ import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
 	Uri struct {
-		Messages      string `envconfig:"URI_MESSAGES" default:"localhost:50051" required:"true"`
+		Reader        string `envconfig:"URI_READER" default:"localhost:50051" required:"true"`
 		Subscriptions string `envconfig:"URI_SUBSCRIPTIONS" default:"localhost:50052" required:"true"`
 		Writer        string `envconfig:"URI_WRITER" default:"localhost:50053" required:"true"`
 	}
--- test/e2e_test.go
+++ test/e2e_test.go
@@ -2,21 +2,14 @@ package test
 
 import (
 	"context"
-	"errors"
-	"github.com/awakari/core/api/grpc/messages"
-	"github.com/awakari/core/api/grpc/subscriptions"
-	"github.com/awakari/core/api/grpc/writer"
+	"github.com/awakari/client-sdk-go/api"
+	"github.com/awakari/client-sdk-go/model"
 	"github.com/awakari/core/test/config"
 	"github.com/awakari/core/test/data"
 	"github.com/cloudevents/sdk-go/binding/format/protobuf/v2/pb"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
-	"io"
 	"testing"
 	"time"
 )
@@ -25,129 +18,118 @@ func Test_MessageDelivery(t *testing.T) {
 	//
 	cfg, err := config.NewConfigFromEnv()
 	require.Nil(t, err)
-	connSubs, err := grpc.Dial(cfg.Uri.Subscriptions, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	require.Nil(t, err)
-	defer connSubs.Close()
-	clientSubs := subscriptions.NewServiceClient(connSubs)
-	connWriter, err := grpc.Dial(cfg.Uri.Writer, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	require.Nil(t, err)
-	defer connWriter.Close()
-	clientWriter := writer.NewServiceClient(connWriter)
 	//
-	mdUser := []string{
-		"X-Awakari-Group-Id", "test-group0",
-		"X-Awakari-User-Id", "test-user0",
-	}
-	ctxUser := metadata.AppendToOutgoingContext(context.TODO(), mdUser...)
+	var client api.Client
+	client, err = api.
+		NewClientBuilder().
+		SubscriptionsUri(cfg.Uri.Subscriptions).
+		WriterUri(cfg.Uri.Writer).
+		ReaderUri(cfg.Uri.Reader).
+		Build()
+	require.Nil(t, err)
+	defer client.Close()
+	//
+	groupIdCtx := metadata.AppendToOutgoingContext(context.TODO(), "x-awakari-group-id", "test-group-0")
 	//
 	var subIds []string
-	for _, sub := range data.Subs {
-		var resp *subscriptions.CreateResponse
-		resp, err = clientSubs.Create(ctxUser, &sub)
-		require.Nil(t, err, err)
-		subIds = append(subIds, resp.Id)
+	for _, subData := range data.Subs {
+		var subId string
+		subId, err = client.CreateSubscription(groupIdCtx, "test-user-0", subData)
+		require.Nil(t, err)
+		subIds = append(subIds, subId)
 	}
 	//
-	var streamMsgsWrite writer.Service_SubmitMessagesClient
-	streamMsgsWrite, err = clientWriter.SubmitMessages(ctxUser)
+	var msgsWriter model.Writer[*pb.CloudEvent]
+	msgsWriter, err = client.OpenMessagesWriter(groupIdCtx, "test-user-1")
 	require.Nil(t, err)
-	err = streamMsgsWrite.Send(&data.Msgs)
-	require.Nil(t, err)
-	var respWrite *writer.SubmitMessagesResponse
-	respWrite, err = streamMsgsWrite.Recv()
-	require.Nil(t, err)
-	require.Equal(t, len(data.Msgs.Msgs), int(respWrite.AckCount))
-	err = streamMsgsWrite.CloseSend()
+	defer msgsWriter.Close()
+	var msgCount uint32
+	msgCount, err = msgsWriter.WriteBatch(data.Msgs)
+	require.Equal(t, len(data.Msgs), int(msgCount))
 	require.Nil(t, err)
 	//
 	cases := map[string]struct {
 		subId string
 		msgs  []*pb.CloudEvent
+		err   error
 	}{
 		"disabled": {
 			subId: subIds[0],
 			msgs:  []*pb.CloudEvent{},
+			err:   context.DeadlineExceeded,
 		},
 		"exact complete value match for a key": {
 			subId: subIds[1],
 			msgs: []*pb.CloudEvent{
-				data.Msgs.Msgs[4],
+				data.Msgs[4],
 			},
 		},
 		"partial exact match": {
 			subId: subIds[2],
 			msgs: []*pb.CloudEvent{
-				data.Msgs.Msgs[0],
+				data.Msgs[0],
 			},
 		},
 		"basic group condition with \"and\" logic and partial sub-conditions": {
 			subId: subIds[3],
 			msgs: []*pb.CloudEvent{
-				data.Msgs.Msgs[1],
+				data.Msgs[1],
 			},
 		},
 		"basic group condition with \"or\" logic": {
 			subId: subIds[4],
 			msgs: []*pb.CloudEvent{
-				data.Msgs.Msgs[2],
-				data.Msgs.Msgs[3],
+				data.Msgs[2],
+				data.Msgs[3],
 			},
 		},
 		"basic group condition with \"and\" logic and a negative sub-condition": {
 			subId: subIds[5],
 			msgs: []*pb.CloudEvent{
-				data.Msgs.Msgs[0],
-				data.Msgs.Msgs[1],
+				data.Msgs[0],
+				data.Msgs[1],
 			},
 		},
 		"single symbol wildcard": {
 			subId: subIds[6],
 			msgs: []*pb.CloudEvent{
-				data.Msgs.Msgs[0],
+				data.Msgs[0],
 			},
 		},
 		"multiple symbol wildcard": {
 			subId: subIds[7],
 			msgs: []*pb.CloudEvent{
-				data.Msgs.Msgs[1],
-				data.Msgs.Msgs[3],
+				data.Msgs[1],
+				data.Msgs[3],
 			},
 		},
 	}
 	//
 	for k, c := range cases {
 		t.Run(k, func(t *testing.T) {
-			var connMsgs *grpc.ClientConn
-			connMsgs, err = grpc.Dial(cfg.Uri.Messages, grpc.WithTransportCredentials(insecure.NewCredentials()))
-			require.Nil(t, err)
-			defer connMsgs.Close()
-			clientMsgs := messages.NewServiceClient(connMsgs)
+			//
 			ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
 			defer cancel()
-			var streamRcv messages.Service_ReceiveClient
-			streamRcv, err = clientMsgs.Receive(ctx)
+			var msgsReader model.Reader[[]*pb.CloudEvent]
+			msgsReader, err = client.OpenMessagesReader(ctx, "test-user-0", c.subId, 4)
 			require.Nil(t, err)
-			err = streamRcv.Send(&messages.ReceiveRequest{Command: &messages.ReceiveRequest_Start{Start: &messages.ReceiveCommandStart{
-				SubId: c.subId,
-			}}})
-			require.Nil(t, err)
-			defer streamRcv.CloseSend()
+			defer msgsReader.Close()
+			//
 			var msgs []*pb.CloudEvent
-			var msg *pb.CloudEvent
+			var msgBatch []*pb.CloudEvent
 			for {
-				msg, err = streamRcv.Recv()
-				if err == io.EOF {
+				msgBatch, err = msgsReader.Read()
+				assert.True(t, err == nil || err == context.DeadlineExceeded)
+				msgs = append(msgs, msgBatch...)
+				select {
+				case <-ctx.Done():
+					err = ctx.Err()
+				default:
+					continue
+				}
+				if err != nil {
 					break
 				}
-				if errors.Is(err, status.Error(codes.DeadlineExceeded, "context deadline exceeded")) {
-					break
-				}
-				require.Nil(t, err, err)
-				err = streamRcv.Send(&messages.ReceiveRequest{Command: &messages.ReceiveRequest_Ack{Ack: &messages.ReceiveCommandAck{
-					Ack: true,
-				}}})
-				require.Nil(t, err, err)
-				msgs = append(msgs, msg)
 			}
 			assert.Equal(t, len(c.msgs), len(msgs))
 			var msgFound bool
